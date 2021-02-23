0379b4dd5fe5e376ce13c704a48f0f0e09dfa5ee
mahi <n.mahesh121@gmail.com>
2021-02-23 14:46:40 +0000

    this is the new application and added first commit

---
+++ helloworld.java
@@ -0,0 +1,8 @@
+// This code will print a statement on command line
+public class Helloworld {
+  public static void main(String[] args) {
+      System.out.println("Hello, World");
+      System.out.println("Welcome to Branching");
+      System.out.println("Welcome to NEWUI Branch");
+  }
+}
